29623094987315b7f443bbd25aa56c0358813a50
Rj <rikrdo.ortega@gmail.com>
2019-08-19 13:42:51 -0600

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,32 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'todo-app-angular';
+  todoArray = []
+  todo
+
+  addTodo(value) {
+    if (value !== "") {
+      this.todoArray.push(value)
+    } else {
+      alert('Field required **')
+    }
+  }
+
+  deleteItem(todo) {
+    for (let i = 0; i <= this.todoArray.length; i++) {
+      if (todo == this.todoArray[i]) {
+        this.todoArray.splice(i, 1)
+      }
+    }
+  }
+
+  todoSubmit(value: any) {
+    if (value !== "") {
+      this.todoArray.push(value.todo)
+    } else {
+      alert('Field required **')
+    }
+  }
 }
